5283d9ebb274992c4f0239da064b4261d3029047
João Barabba <jvbarabba@gmail.com>
2022-02-21 12:16:58 -0300

    Exercicio de condição : IMC atualizado

--- exerc_Aula2.py
+++ exerc_Aula2.py
@@ -2,4 +2,15 @@ altura=float(input("Digite sua altura: "))
 peso=float(input("Digite seu peso: "))
 
 imc=peso/(altura*altura)
-print(f"Seu IMC é de {imc:,.2f}")
+if imc<18.5:
+        print("Abaixo do peso")
+if imc>=18.5 and imc<25:
+        print("Peso normal")
+if imc>=24.9 and imc<30:
+        print("Sobrepeso")
+if imc>=29.9 and imc<35:
+        print("Obesidade 1")
+if imc>=34.9 and imc<40:
+        print("Obesidade 2")
+if imc>40:
+        print("Obesidade 3")
