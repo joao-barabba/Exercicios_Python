61ae69d0b5bab2cb62998ebc09ca58e8973cb6ba
João Barabba <jvbarabba@gmail.com>
2022-03-03 09:42:58 -0300

    Exercicios da Tarefa para entrega

--- Exercicios_entregar/Tarefa_lista3_ex3.py
+++ Exercicios_entregar/Tarefa_lista3_ex3.py
@@ -1,12 +1,15 @@
 #3 - Criar um programa que recebe um depósito inicial e a taxa de juros mensal de uma aplicação e imprime os valores, mês a mês , para os 24 primeiros meses. Além disso, escreve o total ganho com os juros do período.
 
 deposito_inicial=float(input('Digite o valor em R$ de deposito inicial: '))
-tx_juros=float(input('Digite em porcentagem a taxa de juros mensal'))
-c=0
+tx_juros=float(input('Digite em porcentagem a taxa de juros mensal: '))
+mes=0
+calc=tx_juros/100
+valor_atual=deposito_inicial
 
-while c<=24:
-    valor=deposito_inicial*(1+tx_juros/100)
-    print(f'Valor em conta no mês {c} é R${valor:,.2}')
-    c+=1
-ganhos=valor-deposito_inicial
-print(f'Total de ganhos no periodo de 24 meses foi R$ {ganhos:,.2}')    
+while mes<=24:
+    valor_juros=valor_atual*calc
+    valor_atual=valor_atual+valor_juros
+    print(f'Valor em conta no mês {mes} é R${valor_atual:.2f}')
+    mes+=1
+ganhos=valor_atual-deposito_inicial
+print(f'Total de ganhos no periodo de 24 meses foi R$ {ganhos:.2f}')    
